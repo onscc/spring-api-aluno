d93873cc12d39b820bcb0591b54ecd35bb92871e
João Victor Do Nascimento <111939876+onscc@users.noreply.github.com>
2025-06-05 13:21:51 -0300

    Add files via upload

---
+++ src/main/java/com/exemplo/aluno/SpringApiAlunoApplication.java
@@ -0,0 +1,12 @@
+package com.exemplo.aluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiAlunoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiAlunoApplication.class, args);
+    }
+}
